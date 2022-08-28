915932a73b2fab93c960c4b070ef682eb8be6044
jwatts41 <wattsjonovan@yahoo.com>
2022-08-28 19:26:05 -0400

    approaching the chessboard a different way, was unable to add pieces in with the other way

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
-//chessboard pattern creation
-let chessBoard = document.getElementById('chessboard')
-for(let i = 1; i < 9; i++) {
-    let rows = document.createElement('div')
-    rows.classList.add('rows')
-    for(let j = 1; j < 9; j++){
-        let checkers = document.createElement('div')
-        checkers.classList.add('checkers')
-        if(j%2 === 0){
-            checkers.style.background = 'linear-gradient(dark red, red, darkred)'
-        }
-        else{
-            checkers.style.background = 'linear-gradient(gainsboro, silver, gainsboro)'
-        }
-        rows.appendChild(checkers)
-    }
-    chessBoard.appendChild(rows)
-}
+
+
