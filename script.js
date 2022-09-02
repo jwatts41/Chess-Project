9b8fb26c995852db6b24c40b80362528ad9d3586
jwatts41 <wattsjonovan@yahoo.com>
2022-09-01 21:26:32 -0400

    hit a wall

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+//moving the pieces
 
+let pawn = document.getElementsByClassName('Pawn')
+let knight = document.getElementsByClassName('Knight')
+let rook = document.getElementsByClassName('Rook')
+let bishop = document.getElementsByClassName('Bishop')
+let queen = document.getElementsByClassName('Queen')
+let king = document.getElementsByClassName('King')
 
