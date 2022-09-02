05fa21e6cf5f698c9b8e2c0e5a6d844b8fe51950
jwatts41 <wattsjonovan@yahoo.com>
2022-09-02 19:35:54 -0400

    working on the movement of the pieces

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-//moving the pieces
+// each pieces moveset, used Techy Web Dev on Youtube for guidance
 
-let pawn = document.getElementsByClassName('Pawn')
-let knight = document.getElementsByClassName('Knight')
-let rook = document.getElementsByClassName('Rook')
-let bishop = document.getElementsByClassName('Bishop')
-let queen = document.getElementsByClassName('Queen')
-let king = document.getElementsByClassName('King')
 
