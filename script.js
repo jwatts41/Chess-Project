58f53fab2ec9d480089ddb8c9bf5624d8ef55a03
jwatts41 <wattsjonovan@yahoo.com>
2022-09-02 21:43:43 -0400

    pieces move but board is distorted in process, trying another way

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
-// each pieces moveset, used Techy Web Dev on Youtube for guidance
+// moving the pieces sourced from W3 schools
+/* Events fired on the drag target */
 
-
+document.addEventListener("dragstart", function(event) {
+ 
+    // The dataTransfer.setData() method sets the data type and the value of the dragged data
+   event.dataTransfer.setData("Text", event.target.id);
+   
+   // Change the opacity of the draggable element
+   event.target.style.opacity = "0.4";
+ });
+ 
+ // Output some text when finished dragging the p element and reset the opacity
+ document.addEventListener("dragend", function(event) {
+   event.target.style.opacity = "1";
+ });
+ 
+ /* Events fired on the drop target */
+ 
+ // When the draggable p element enters the droptarget, change the DIVS's border style
+ document.addEventListener("dragenter", function(event) {
+   if ( event.target.className == "red" || "silver" ) {
+     event.target.style.border = "3px dotted red";
+   }
+ });
+ 
+ // By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element
+ document.addEventListener("dragover", function(event) {
+   event.preventDefault();
+ });
+ 
+ // When the draggable p element leaves the droptarget, reset the DIVS's border style
+ document.addEventListener("dragleave", function(event) {
+   if ( event.target.className == "red" || "silver" ) {
+     event.target.style.border = "";
+   }
+ });
+ 
+ /* On drop - Prevent the browser default handling of the data (default is open as link on drop)
+    Reset the color of the output text and DIV's border color
+    Get the dragged data with the dataTransfer.getData() method
+    The dragged data is the id of the dragged element ("drag1")
+    Append the dragged element into the drop element
+ */
+ document.addEventListener("drop", function(event) {
+   event.preventDefault();
+   if ( event.target.className == "red" || "silver" ) {
+     event.target.style.border = "";
+     var data = event.dataTransfer.getData("Text");
+     event.target.appendChild(document.getElementById(data));
+   }
+ });
