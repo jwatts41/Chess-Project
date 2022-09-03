a6a71ff7ded7625b7d63728e1e29636eadcf28e9
jwatts41 <wattsjonovan@yahoo.com>
2022-09-03 01:35:16 -0400

    finally wrote the correct code to move a piece after 3 days lol

--- script.js
+++ script.js
@@ -1,61 +1,18 @@
-// moving the pieces sourced from W3 schools
+// moving the pieces sourced from mdn web docs
 
-/* Events fired on the drag target */
+function allowDrop(allowdropevent) {
+  allowdropevent.target.style.color = 'blue';
+  allowdropevent.preventDefault();
+}
 
-document.addEventListener("dragstart", function(event) {
-  // The dataTransfer.setData() method sets the data type and the value of the dragged data
-  event.dataTransfer.setData("Text", event.target.id);
-  
-  
-  
-  // Change the opacity of the draggable element
-  event.target.style.opacity = "0.4";
-});
+function drag(dragevent) {
+  dragevent.dataTransfer.setData("text", dragevent.target.id);
+  dragevent.target.style.color = 'green';
+}
 
-// While dragging the p element, change the color of the output text
-document.addEventListener("drag", function(event) {
-  document.getElementById("demo").style.color = "red";
-});
-
-// Output some text when finished dragging the p element and reset the opacity
-document.addEventListener("dragend", function(event) {
-  document.getElementById("demo").innerHTML 
-  event.target.style.opacity = "1";
-});
-
-/* Events fired on the drop target */
-
-// When the draggable p element enters the droptarget, change the DIVS's border style
-document.addEventListener("dragenter", function(event) {
-  if ( event.target.className == "droptarget" ) {
-    event.target.style.border = "3px dotted red";
-  }
-});
-
-// By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element
-document.addEventListener("dragover", function(event) {
-  event.preventDefault();
-});
-
-// When the draggable p element leaves the droptarget, reset the DIVS's border style
-document.addEventListener("dragleave", function(event) {
-  if ( event.target.className == "droptarget" ) {
-    event.target.style.border = "";
-  }
-});
-
-/* On drop - Prevent the browser default handling of the data (default is open as link on drop)
-   Reset the color of the output text and DIV's border color
-   Get the dragged data with the dataTransfer.getData() method
-   The dragged data is the id of the dragged element ("drag1")
-   Append the dragged element into the drop element
-*/
-document.addEventListener("drop", function(event) {
-  event.preventDefault();
-  if ( event.target.className == "droptarget" ) {
-    document.getElementById("demo").style.color = "";
-    event.target.style.border = "";
-    var data = event.dataTransfer.getData("Text");
-    event.target.appendChild(document.getElementById(data));
-  }
-});
+function drop(dropevent) {
+  dropevent.preventDefault();
+  const data = dropevent.dataTransfer.getData("text");
+  dropevent.target.appendChild(document.getElementById(data));
+  document.getElementById("drag").style.color = 'black';
+}
