7c8447de4896941d8576a07235d33a686a2d31c8
jwatts41 <wattsjonovan@yahoo.com>
2022-09-09 21:03:32 -0400

    working on the ascii table

--- script.js
+++ script.js
@@ -24,6 +24,21 @@ function confirmBoardRefresh() {
   }
 }
 
+//ascII values sourced from asciitable.com and programiz.com 
+
+// program to find the ASCII value of a character
+
+// take input from the user
+const string = prompt('Enter a character: ');
+
+// convert into ASCII value
+const result = string.codePointAt(0);
+
+console.log(`The ASCII value is: ${result}`);
+
+
+
+
 
 
 
